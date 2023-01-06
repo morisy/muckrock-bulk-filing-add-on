64663f2dc164bf8edaddcd9983dade2c99a1031f
Michael Morisy <m@morisy.com>
2023-01-06 14:05:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,18 +21,18 @@ class BulkFile(AddOn):
         """The main add-on functionality goes here."""
         # fetch your add-on specific data
 
-	credentials = {
-	    "installed": {
-		"client_id": os.environ["google_client_id"],
-		"project_id": os.environ["google_project_id"],
- 		"auth_uri":"https://accounts.google.com/o/oauth2/auth",
-		"token_uri":"https://oauth2.googleapis.com/token",
-		"auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","
-		"client_secret":os.environ["google_client_secret"],
-		"redirect_uris":"http://localhost"
-	    }
-	}
-	gc, authorized_user = gspread.oauth_from_dict(credentials)
+        credentials = {
+                "installed": {
+                "client_id": os.environ["google_client_id"],
+                "project_id": os.environ["google_project_id"],
+                "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+                "token_uri":"https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","
+                "client_secret":os.environ["google_client_secret"],
+                "redirect_uris":"http://localhost"
+                }
+        }
+        gc, authorized_user = gspread.oauth_from_dict(credentials)
 
         
         row = 2 # skip the first row, which is reserved for headers
